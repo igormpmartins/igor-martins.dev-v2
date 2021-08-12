bff7688f1c7549f118ff2c475624ec70136577dc
igormpmartins <73676465+igormpmartins@users.noreply.github.com>
2021-08-12 20:54:55 -0300

    fix responsive

--- Components/Hero/index.js
+++ Components/Hero/index.js
@@ -5,8 +5,8 @@ const Hero = () => {
     return (
         <div className='md:grid md:grid-cols-2 md:pt-16 leading-none'>
                 <div className='pt-8 md:pt-32'>
-                    <h1 className='text-4xl uppercase pl-16'>Igor Martins</h1>
-                    <h2 className='font-bold text-5xl uppercase pl-16'>Fullstack Developer</h2>
+                    <h1 className='text-2xl md:text-4xl uppercase text-center md:text-left md:pl-16'>Igor Martins</h1>
+                    <h2 className='font-bold text-3xl md:text-5xl text-center md:text-left uppercase md:pl-16'>Fullstack Developer</h2>
                     <div className='border rounded-lg border-rose px-8 mx-8 md:mx-0 md:px-16 pb-4 mt-10 md:mt-6 text-center md:text-left'>
                         <h3 className='text-2xl text-white bg-rose bold uppercase -mt-5 table px-6 py-1'>Contact me</h3>
                         <p className='text-4xl md:text-6xl inline-block mt-4 text-center md:text-left'>
