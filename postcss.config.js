1edadc22f796679042da658905e9910d069180b8
igormpmartins <73676465+igormpmartins@users.noreply.github.com>
2021-08-12 20:47:37 -0300

    fix purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,30 +1,7 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-  }
-];
-
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === "production"
-    ? {
-        "@fullhuman/postcss-purgecss": {
-          content: [
-            "./components/**/*.*",
-            "./pages/**/*.*",
-            "./contexts/**/*.*",
-            "./gql/**/*.*",
-            "./utils/**/*.*",
-            "./public/**/*.*",
-          ],
-          defaultExtractor: (content) =>
-            content.match(/[\w-/.:]+(?<!:)/g) || [],
-        },
-      }
-    : {}),
-  },
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer'
+  ],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge:  [
+    "./components/**/*.js", 
+    "./pages/**/*.js"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
