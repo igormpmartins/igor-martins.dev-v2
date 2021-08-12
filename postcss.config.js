abddd83e14efd08496c7e3cfa0104619e47bb280
igormpmartins <73676465+igormpmartins@users.noreply.github.com>
2021-08-12 17:44:25 -0300

    fix purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -10,6 +10,21 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    ...(process.env.NODE_ENV === "production"
+    ? {
+        "@fullhuman/postcss-purgecss": {
+          content: [
+            "./components/**/*.*",
+            "./pages/**/*.*",
+            "./contexts/**/*.*",
+            "./gql/**/*.*",
+            "./utils/**/*.*",
+            "./public/**/*.*",
+          ],
+          defaultExtractor: (content) =>
+            content.match(/[\w-/.:]+(?<!:)/g) || [],
+        },
+      }
+    : {}),
   },
 }
