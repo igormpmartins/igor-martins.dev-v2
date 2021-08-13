69bbd2b0d88e76099c34c3400d0d88e08be322b0
igormpmartins <73676465+igormpmartins@users.noreply.github.com>
2021-08-13 20:03:58 -0300

    fix links

--- Components/Hero/ContactMe.js
+++ Components/Hero/ContactMe.js
@@ -6,10 +6,10 @@ const ContactMe = () => {
         <div className='border rounded-lg border-rose px-8 mx-8 md:mx-0 md:px-16 pb-4 mt-10 md:mt-6 text-center md:text-left'>
                         <h3 className='text-2xl text-white bg-rose bold uppercase -mt-5 table px-6 py-1'>Contact me</h3>
                         <p className='text-4xl md:text-6xl inline-block mt-4 text-center md:text-left'>
-                            <GrLinkedin className='inline-block mr-4'/>
-                            <GrGithub className='inline-block mr-4'/>
-                            <GrInstagram className='inline-block mr-4'/>
-                            <GrYoutube className='inline-block mr-4'/>
+                            <a href='https://www.linkedin.com/in/igormpmartins'><GrLinkedin className='inline-block mr-4'/></a> 
+                            <a href='https://www.github.com/igormpmartins'><GrGithub className='inline-block mr-4'/></a>
+                            <a href='https://www.instagram.com/igormpmartins'><GrInstagram className='inline-block mr-4'/></a>
+                            <a href='https://www.youtube.com/channel/UCfe4arqMt1-adwcsYmbvmhA'><GrYoutube className='inline-block mr-4'/></a>
                         </p>
                         <p className='mt-4'>or drop a line: igormpmartins@gmail.com</p>
                     </div>
